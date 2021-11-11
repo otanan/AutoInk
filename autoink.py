0a9b7df41942e1cd04f81e58c302c4565f88f633
Jonathan Delgado <otanan@Jonathans-MacBook-Pro-2.local>
2021-11-11 00:29:17 -0800

    Updated to include edit figure hotkey

--- autoink.py
+++ autoink.py
@@ -7,19 +7,19 @@
 #------------- Imports -------------#
 import sublime, sublime_plugin
 
-# Paths (no pathlib) and copying files
-import os, shutil
-# Running Bash/Applescript commands
-import subprocess
-from subprocess import Popen, PIPE
-
+from pathlib import Path
+import shutil
 # File searching
 import glob
+# Running Bash commands
+import subprocess
 #------------- End imports -------------#
 
-template_path = os.path.dirname(__file__) + '/template.svg'
-
 #------------- Settings -------------#
+
+_F_EXT = '.svg'
+
+template_path = (Path(__file__).parent / 'template').with_suffix(_F_EXT)
 
 default_possible_fig_folder_names = [
     'figures',
@@ -56,7 +56,7 @@ def get_line(view):
     return view.line(view.sel()[0])
 
 
-def get_line_text(view):
+def read_line(view):
     """ Gets the full line of text at the current cursor position. """
     return view.substr(get_line(view))
 
@@ -68,13 +68,13 @@ def line_to_caption(line):
 
 def replace_current_line(view, edit, text):
     """ Replace current line of text. """
-    # view.insert(edit, view.sel()[0].begin(), text)
     view.replace(edit, get_line(view), text)
 
 
 def get_current_folder(view):
     """ Gets the current folder the file being edited is in. """
-    return os.path.abspath(str(view.file_name()) + '/../')
+    # return os.path.abspath(str(view.file_name()) + '/../')
+    return Path(view.file_name()).parent
 
 
 def find_figures_folder(view, recursive_check, possible_fig_folder_names):
@@ -91,15 +91,13 @@ def find_figures_folder(view, recursive_check, possible_fig_folder_names):
         # Run through each parent folder to search for figures folder
         for fig_folder_name in possible_fig_folder_names:
             # Go through possible folder names
-            path = os.path.abspath(
-                current_folder + '/' + fig_folder_name
-            )
+            path = Path(current_folder) / fig_folder_name
 
-            if os.path.isdir(path):
+            if path.is_dir():
                 # Found the figures folder
                 return path
 
-        current_folder += '/..'
+        current_folder /= '..'
 
     # No folder could be found.
     return None
@@ -108,7 +106,8 @@ def find_figures_folder(view, recursive_check, possible_fig_folder_names):
 ######################## Main ########################
 
 
-class AutoInkCommand(sublime_plugin.TextCommand):
+class NewAutoInkCommand(sublime_plugin.TextCommand):
+    """ Takes line to create new Inkscape figure. """
 
     def run(self, edit):
         print('Running Figure Command...\n')
@@ -120,13 +119,12 @@ class AutoInkCommand(sublime_plugin.TextCommand):
             'figures_folders', default_possible_fig_folder_names
         )
         
-
         recursive_check = settings.get(
             'recursive_check', default_recursive_check
         )
 
         # Get the line to parse out the new figure name
-        line = get_line_text(view)
+        line = read_line(view)
         figure_name = line_to_fname(line)
         # The raw line will be used to caption the figure.
         caption = line_to_caption(line)
@@ -140,28 +138,62 @@ class AutoInkCommand(sublime_plugin.TextCommand):
             # Make one
             current_folder = get_current_folder(view)
             # Call it the top ranked folder
-            figures_folder = current_folder + '/' + possible_fig_folder_names[0]
-            os.mkdir(figures_folder)
+            figures_folder = current_folder / possible_fig_folder_names[0]
+            figures_folder.mkdir()
 
         ### Paste the LaTeX command ###
         command = latex_command.format(
-            file_name='{' + figure_name + '}',
+            file_name=f'{{{figure_name}}}',
             # Caption the figure with the raw line adjusted
-            caption='{' + caption + '}',
+            caption=f'{{{caption}}}',
             # Label the figure with the same name as the file
-            fig_name='{fig:' + figure_name + '}',
+            fig_name=f'{{fig:{figure_name}}}',
         )
         replace_current_line(view, edit, command)
 
         # Search the figures path to find existing figures with same name
-        target_path = figures_folder + '/' + figure_name
-        files = glob.glob(target_path + '.svg')
+        files = list(figures_folder.glob(f'{figure_name}{_F_EXT}'))
 
         if len(files) == 0:
             print('No figures found, copying template.')
             # If no, copy figure template to figure folder with correct name
-            shutil.copy(template_path, target_path + '.svg')
+            fname = (figures_folder / figure_name).with_suffix(_F_EXT)
+            shutil.copy(template_path, fname)
             # Launches the app with the copied figure file for editing
-            subprocess.Popen(['inkscape', target_path + '.svg'])
+            subprocess.Popen(['inkscape', fname])
 
         print('Finished.')
+
+
+class EditAutoInkCommand(sublime_plugin.WindowCommand):
+
+    def run(self):
+        view = self.window.active_view()
+
+        # Load settings
+        settings = sublime.load_settings("AutoInk.sublime-settings")
+
+        possible_fig_folder_names = settings.get(
+            'figures_folders', default_possible_fig_folder_names
+        )
+        
+        recursive_check = settings.get(
+            'recursive_check', default_recursive_check
+        )
+
+
+        figures_folder = find_figures_folder(view, recursive_check, possible_fig_folder_names)
+        files = list(figures_folder.glob(f'*{_F_EXT}'))
+        fnames = [
+            file.stem
+            for file in files
+        ]
+        
+        # Show list of possible files
+        self.window.show_quick_panel(
+            fnames, lambda choice_index: self.on_done(choice_index, files)
+        )
+
+    def on_done(self, choice_index, files):
+        path = files[choice_index]
+        subprocess.Popen(['inkscape', path])
